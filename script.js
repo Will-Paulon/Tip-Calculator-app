36e47a79f2c30074e013b33cd603972f91a67992
Willian Alves Paulon <willianpaulon50@gmail.com>
2023-05-10 23:59:01 -0300

    Construção JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('teste')
+
+
+
+
+
+function calcular() {
+    var tn1 = window.document.getElementById('txtn1')
+    var tn2 = window.document.getElementById'('txtn2')
+    var tn3 = window.document.getElementById('txtn3')
+
+
+
+
+
+
+}
