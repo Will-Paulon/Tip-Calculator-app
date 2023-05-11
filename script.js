ead1084360ba67bd995d5bf76ba86a1e76b2f41d
Willian Alves Paulon <willianpaulon50@gmail.com>
2023-05-11 00:57:26 -0300

    Em Construção Js

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+const form = document.querySelector("#tip")
+const bill = document.querySelector('#txtn1')
+const custom = document.querySelector('#txtn2')
+const nPeople = document.querySelector('#txtn3') 
+const send = document.querySelector('#SendBnt') 
+const reset = document.querySelector('#resetBnt') 
+const tipVal = document.querySelector("#tipValue")
+const personBill = document.querySelector("#personBill")
 
+let billV  = 0;
+let qtdPeople = 0;
+let tip = 10/100;
+let calc = 0;
 
+//****Envio de valores*****//
 
+send.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (validate(bill.value, nPeople.value, "billEmpty","peapleEmpty")){
+        billV = parseFloat(bill.value)
+        qtdPeople = parseFloat(nPeople.value)
 
+        tipVal.innerHTML = currency((billV * tip)/qtdPeople)
+        personBill.innerHTML = currency((billV + (billV * tip))/qtdPeople)
+        clearValue()
+        if (customTip.value !== ''){
+            tip = parseInt(custom.value) / 100
+        }
+    }
+})
 
-function calcular() {
-    var tn1 = window.document.getElementById('txtn1')
-    var tn2 = window.document.getElementById'('txtn2')
-    var tn3 = window.document.getElementById('txtn3')
-
-
-
-
-
-
-}
